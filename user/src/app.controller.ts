ebfacfc5095a31dc9cbb51a4ba04282be58d39af
Rahmat Yani Hidayat <mortarboyz@gmail.com>
2022-03-09 14:29:07 +0700

    feat: user message pattern

--- user/src/app.controller.ts
+++ user/src/app.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
+import { MessagePattern } from '@nestjs/microservices';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +9,13 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @MessagePattern('redis-test')
+  async redisTest(data: any) {
+    return {
+      status: true,
+      message: 'Hello Microservice!'
+    };
   }
 }
