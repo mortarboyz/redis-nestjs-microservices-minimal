681d5534e38fbc082ca753e7f2baef0d5a456d21
Rahmat Yani Hidayat <mortarboyz@gmail.com>
2022-03-09 14:38:51 +0700

    chore: setup microservice core

--- core/src/app.module.ts
+++ core/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'USER_SERVICE',
+        transport: Transport.REDIS,
+        options: {
+          url: 'redis://192.168.0.107:6379',
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
