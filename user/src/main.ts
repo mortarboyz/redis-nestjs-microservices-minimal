9816bae4bae3263f4cbb54882153c8f48a6b18e4
Rahmat Yani Hidayat <mortarboyz@gmail.com>
2022-03-09 14:27:08 +0700

    chore: setup microservice redis

--- user/src/main.ts
+++ user/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.connectMicroservice({
+    transport: Transport.REDIS,
+    options: {
+      url: 'redis://192.168.0.107:6379',
+    },
+  });
+  await app.startAllMicroservices();
+  await app.listen(5000);
 }
 bootstrap();
